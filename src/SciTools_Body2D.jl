35c738cd91e787bf64326a284ea7f33446d33c5d
camperD <damyn.chipman@gmail.com>
2019-01-18 13:52:12 -0700

    Fixing Body2D

--- src/SciTools_Body2D.jl
+++ src/SciTools_Body2D.jl
@@ -16,11 +16,12 @@ function Body2D(X::Array{Float64},
     # Iterate around body to create geometry
     NPTS = length(X)
     NPAN = NPTS - 1
-    body = Array{LineSegment}(NPAN)
-    for n=1:NPTS-1
-        r1 = [X[n];Y[n]]
-        r2 = [X[n+1];Y[n+1]]
-        body[n] = LineSegment(r1,r2)
-    end
+    body = [LineSegment([X[n], Y[n]], [X[n+1], Y[n+1]]) for n = 1:NPTS-1]
+    # body = Array{LineSegment,NPAN}
+    # for n=1:NPTS-1
+    #     r1 = [X[n];Y[n]]
+    #     r2 = [X[n+1];Y[n+1]]
+    #     body[n] = LineSegment(r1,r2)
+    # end
     return body
 end
